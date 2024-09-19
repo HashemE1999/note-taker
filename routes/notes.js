7ca56014fca6c0a43779bc801e98ef7f7ce6ad53
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-09-19 13:04:44 -0400

    Changes to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -2,17 +2,21 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/', (req, res) => {
     // Read the db.json file
     const notes = readNotes();
     // Return as JSON data
-    
+    res.json(notes);
 });
 
 router.post('/', (req, res) => {
-    
-})
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    saveNote(newNote);
+    res.json({message: 'Note saved successfully!'});
+});
 
 router.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/index.html'))
