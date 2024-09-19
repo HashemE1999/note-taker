4d3863f9a57ad45a7d7dc0de11cec57bd46c7ebd
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-09-19 12:40:23 -0400

    Changes to routes, server, and js files

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+// Importing router
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+router.get('/', (req, res) => {
+    // Read the db.json file
+    const notes = readNotes();
+    // Return as JSON data
+    
+});
+
+router.post('/', (req, res) => {
+    
+})
+
+router.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+// Reads notes from db.json and parses JSON data
+function readNotes() {
+    const data = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8');
+    return JSON.parse(data);
+}
+
+// Saves a new note by reading the existing notes, adding the new note, and writing the updated array back to db.json
+function saveNote(note) {
+    const notes = readNotes();
+    notes.push(note);
+    fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes, null, 2));
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+// Requires fs to read db.json file
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const api = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);  
